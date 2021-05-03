58e019b3e81467b3c177664e96634f280a6c932d
Devon Poston <dev.poston@gmail.com>
2021-05-02 19:41:38 -0700

    all hover animations working

--- client/src/components/ArrivesBy.jsx
+++ client/src/components/ArrivesBy.jsx
@@ -5,7 +5,7 @@ import Moment from 'react-moment';
 const ArrivesBy = (props) => (
   <div className="shipbox">
     <img className="img" src="https://www.pngitem.com/pimgs/m/483-4838442_truck-icon-truck-vector-png-transparent-png.png"></img>
-    <h6 className="shiptext">Arrives by May 5-8 if you order today.<br/>Hooray! This item ships free to the US.</h6>
+    <h6 className="shiptext"><b>Arrives by <u>May 5-8</u></b> if you order today.<br/><b>Hooray!</b> This item ships free to the US.</h6>
   </div>
 );
 
--- client/src/components/OthersFavorited.jsx
+++ client/src/components/OthersFavorited.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OthersFavorited = (props) => (
   <div className="cartbox">
-    <img className="img" src="https://www.pngitem.com/pimgs/m/522-5227702_shopping-cart-of-checkered-design-shopping-cart-vector.png"></img><h6 className="carttext">Other people want this. Over 20 people have this in their carts right now.</h6>
+    <img className="img" src="https://www.pngitem.com/pimgs/m/522-5227702_shopping-cart-of-checkered-design-shopping-cart-vector.png"></img><h6 className="carttext"><b>Other people want this.</b> Over 20 people have this in their carts right now.</h6>
   </div>
 );
 
