efe781363e3b9ac46157a72788ff22ffc2c53a23
Devon Poston <dev.poston@gmail.com>
2021-05-06 15:46:57 -0700

    render tooltip box for approx delivery date on hover  Please enter the commit message for your changes. Lines starting

--- client/src/components/ArrivesBy.jsx
+++ client/src/components/ArrivesBy.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import moment from 'moment';
-import Moment from 'react-moment';
+import ShipByInfo from './ShipByInfo.jsx';
 
-const ArrivesBy = (props) => (
-  <div className="shipbox">
-    <img className="img" src="https://www.pngitem.com/pimgs/m/483-4838442_truck-icon-truck-vector-png-transparent-png.png"></img>
-    <h6 className="shiptext"><b>Arrives by <u>May 5-8</u></b> if you order today.<br/><b>Hooray!</b> This item ships free to the US.</h6>
-  </div>
-);
+const ArrivesBy = (props) => {
+  let timestamp = moment(Date.now()).add(6, 'days').format("MMM Do YY").slice(0, -5);
+  let timestampRange = Number(timestamp.slice(-2)) + 3;
+  return (
+    <div className="shipbox">
+      <img className="img" src="https://www.pngitem.com/pimgs/m/483-4838442_truck-icon-truck-vector-png-transparent-png.png"></img>
+      <h6 className="shiptext"><b>Arrives by <div className="shiptime"><u>{timestamp}-{timestampRange}</u><ShipByInfo /></div></b> if you order today.<br /><b>Hooray!</b> This item ships free to the US.</h6>
+    </div>
+  )
+};
 
 export default ArrivesBy;
---
+++ client/src/components/ShipByInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ShipByInfo = (props) => (
+  <div className="shiptime">
+    <span className="shiptimetext">The estimated delivery date is based on your purchase date, the recipient's location (actual or inferred), the seller's processing time and location, and the shipping carrier.<br/><br/>Other factors—such as shipping carrier delays or placing an order on weekend/holiday—may push the arrival of your item beyond this date.<br/><br/><u>Learn more</u></span>
+  </div>
+);
+
+export default ShipByInfo;
