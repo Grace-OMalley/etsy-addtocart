fbc2c418650cb03af0936be7db746304276ce01e
Devon Poston <dev.poston@gmail.com>
2021-05-11 18:43:20 -0700

    added sampledataapi call

---
+++ sampleDataAPI.js
@@ -0,0 +1,23 @@
+//url
+'https://localhost:3000/addToCart'
+
+{
+  specifications: [
+    ['Extra Small'],
+    ['Small'],
+    ['Medium'],
+    ['Large'],
+    ['Extra Large']
+  ],
+  _id: 6088edca4912fe3bada3375f,
+  brandName: 'Jones - Morar',
+  numberOfSales: 53156,
+  reviewAvg: 3,
+  itemName: 'Incredible Concrete Chicken',
+  price: 297,
+  inventoryCount: 11656,
+  shippingHandlingTime: 1620796106543,
+  itemDetails: 'Metal',
+  itemDescription: 'The beautiful range of Apple Naturalé that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients',
+  __v: 0
+}
