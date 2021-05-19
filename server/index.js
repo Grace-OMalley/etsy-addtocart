971418ff9e0a153036ad34a9bf2b7cfc694b7275
Devon Poston <dev.poston@gmail.com>
2021-05-19 13:21:29 -0700

    refactor after reseeding

--- server/index.js
+++ server/index.js
@@ -11,32 +11,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/addtocart', (req, res) => {
-  for (let i = 1; i < 101; i++) {
-    let dummyData = {
-      itemId: i,
-      brandName: faker.company.companyName(),
-      numberOfSales: faker.datatype.number(),
-      reviewAvg: Math.floor(Math.random() * 6),
-      itemName: faker.commerce.productName(),
-      price: faker.commerce.price(),
-      specifications: ['Extra Small', 'Small', 'Medium', 'Large', 'Extra Large'],
-      inventoryCount: faker.datatype.number(),
-      itemDetails: faker.commerce.productMaterial(),
-      itemDescription: faker.commerce.productDescription()
-    };
-    db.save(dummyData, (err, data) => {
-      console.log('SERVER - Saved to DB...');
-    });
-  }
-  // let query = {};
-  // db.find(query, (err, data) => {
-  //   if (err) {
-  //     res.status(400).send(err);
-  //   } else {
-  //     console.log('SERVER GET DATA:', data[1]);
-  //     res.status(200).send(data[1]);
-  //   }
-  // });
+  //=====================SEEDING FUNCTION============================//
+  // for (let i = 1; i < 101; i++) {
+  //   let dummyData = {
+  //     itemId: i,
+  //     brandName: faker.company.companyName(),
+  //     numberOfSales: faker.datatype.number(),
+  //     reviewAvg: Math.floor(Math.random() * 6),
+  //     itemName: faker.commerce.productName(),
+  //     price: faker.commerce.price(),
+  //     specifications: ['Extra Small', 'Small', 'Medium', 'Large', 'Extra Large'],
+  //     inventoryCount: faker.datatype.number(),
+  //     itemDetails: faker.commerce.productMaterial(),
+  //     itemDescription: faker.commerce.productDescription()
+  //   };
+  //   db.save(dummyData, (err, data) => {
+  //     console.log('SERVER - Saved to DB...');
+  //   });
+  // }
+  //===============================================================//
+  let query = {};
+  db.find(query, (err, data) => {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      console.log('SERVER GET DATA:', data[1]);
+      res.status(200).send(data[1]);
+    }
+  });
 });
 
 app.post('/addtocart', (req, res) => {
